a7b1b12d19b84d330f715f48c812bf494e209153
= <=>
2016-03-29 22:32:03 -0700

    CRUD and validations

--- OdeToFood/Controllers/ReviewController.cs
+++ OdeToFood/Controllers/ReviewController.cs
@@ -1,4 +1,5 @@
 ﻿using OdeToFood.Models;
+using System.Data.Entity;
 using System.Web.Mvc;
 
 namespace OdeToFood.Controllers
@@ -7,14 +8,49 @@ namespace OdeToFood.Controllers
     {
         OdeToFoodDb _db = new OdeToFoodDb();
 
-        public ActionResult Index([Bind(Prefix = "id")]int restaurantId)
+        public ActionResult Index([Bind(Prefix = "id")] int restaurantId)
         {
             var restaurant = _db.Restaurants.Find(restaurantId);
-            if (restaurantId != null)
+            if (restaurant != null)
             {
                 return View(restaurant);
             }
             return HttpNotFound();
+        }
+
+        public ActionResult Create(int restaurantId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Reviews.Add(review);
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
+        }
+
+        public ActionResult Edit(int Id)
+        {
+            var model = _db.Reviews.Find(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(RestaurantReview review)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Entry(review).State = EntityState.Modified;
+                _db.SaveChanges();
+                return RedirectToAction("Index", new { id = review.RestaurantId });
+            }
+            return View(review);
         }
 
         protected override void Dispose(bool disposing)
--- OdeToFood/Models/RestaurantReview.cs
+++ OdeToFood/Models/RestaurantReview.cs
@@ -1,11 +1,21 @@
-﻿namespace OdeToFood.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OdeToFood.Models
 {
     public class RestaurantReview
     {
         public int Id { get; set; }
+
+        [Range(0,10)]
+        [Required]
         public int Rating { get; set; }
+
+        [Required]
+        [MaxLength(256)]
         public string Body { get; set; }
+
         public string ReviewerName { get; set; }
+
         public int RestaurantId { get; set; }
     }
 }
