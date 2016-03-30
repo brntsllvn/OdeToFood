30075cea90d106c0f31e1fd4e5579c202e3bf7a0
= <=>
2016-03-29 18:01:18 -0700

    view a restaurant's reviews

---
+++ OdeToFood/Controllers/ReviewController.cs
@@ -0,0 +1,26 @@
+﻿using OdeToFood.Models;
+using System.Web.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    public class ReviewController : Controller
+    {
+        OdeToFoodDb _db = new OdeToFoodDb();
+
+        public ActionResult Index([Bind(Prefix = "id")]int restaurantId)
+        {
+            var restaurant = _db.Restaurants.Find(restaurantId);
+            if (restaurantId != null)
+            {
+                return View(restaurant);
+            }
+            return HttpNotFound();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
